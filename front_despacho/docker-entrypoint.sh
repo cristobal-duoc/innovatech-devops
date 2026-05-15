5095a4af5f189e2dfd000462d38a97b7888f4071
CristobalMartinez <cristobal.murt@gmail.com>
2026-05-15 18:20:27 -0400

    fix: nginx conf en /tmp para evitar permisos en /etc/nginx/conf.d

--- front_despacho/docker-entrypoint.sh
+++ front_despacho/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
-# Sustituye IPs de backend en plantilla nginx y arranca
+# Sustituye IPs de backend y escribe en /tmp (siempre escribible)
 envsubst '${BACKEND_DESPACHOS} ${BACKEND_VENTAS}' \
   < /etc/nginx/conf.d/default.conf.template \
-  > /etc/nginx/conf.d/default.conf
+  > /tmp/nginx-conf/default.conf
 exec nginx -g "daemon off;"
